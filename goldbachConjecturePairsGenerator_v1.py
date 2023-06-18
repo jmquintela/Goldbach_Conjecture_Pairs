1d503d4d04fb4e5d607c0b8accfb6e50a3f75143
Kinte <jmquintela@gmail.com>
2023-06-18 06:25:59 -0400

    Update goldbachConjecturePairsGenerator_v1.py

--- goldbachConjecturePairsGenerator_v1.py
+++ goldbachConjecturePairsGenerator_v1.py
@@ -1,4 +1,5 @@
 import time
+from random import randrange
 
 import numpy as np
 
@@ -7,21 +8,26 @@ def nearestEqualOrSmallerIndex(n:int, indexSlicers, initialL, debug=False):
   
   n2 = 0
   l = initialL[indexSlicers[0]:indexSlicers[1]]
+  
   if debug:
       print("index Slicers: {} \n\n l: {} \n\n n : {} \n\n ".format(indexSlicers,l,n))     
   
   for x,v in enumerate(l):
+  
     IfEqual = (v == n)
     IfLess = (v < n)
     IfBigger = (v > n)
+
     index = indexSlicers[0] + x 
-    v2 = initialL[index]  
-    
+        
     if debug:
+      
       conditions = []
+      v2 = initialL[index]  
       conditions.append(IfEqual)
       conditions.append(IfLess)
       conditions.append(IfBigger)
+      
       print("conditions : {} \n\n n: {} \n x: {} \n index: {} \n value loop: {} \n value on list: {} ".format(conditions,n,x,index,v,v2))   
    
     if IfEqual:
@@ -34,65 +40,75 @@ def nearestEqualOrSmallerIndex(n:int, indexSlicers, initialL, debug=False):
     if IfBigger:
         n2 = index-1    
         break  
-  
   if debug:
       print("n final: {} ".format(n2))  
-
-  return n2  
+ 
+  return n2
+  #Give us the last appenden object 
        
-def equalorSmallerIndexOnListToN(n:int, l:list, debug=False):
-    
+def equalOrSmallerIndexOnListToN(n:int, l:list, debug=False):  
     # l is a sorted list
+    
     lenL = len(l)
     leftSlice = 0
     rightSlice = lenL
     indexSlicers=[leftSlice,rightSlice]
     initialL = l
     d = 100
-  
-    def findNearNumberbyHalfingSignComparison(d:int,n:int,lenL:int,indexSlicers:list, initialL:list,debug=False) -> list:
+    
+    def FindNearNumberbyHalfingSignComparison(d:int,n:int,lenL:int,indexSlicers:list, initialL:list,debug=False) -> list:
              
       ifNBiggerThanlenL = ( initialL[-1] <= n)    
       if ifNBiggerThanlenL:    
-        return lenL-1
+        return lenL-1        
       ifLenNLessorEqualltoD = (lenL <= d)
+       
       if  ifLenNLessorEqualltoD:    
         #we short the loop when we reach d element        
         return nearestEqualOrSmallerIndex(n,indexSlicers,initialL,debug)          
-            
-      half = int(lenL/2)
-      index = indexSlicers[0] + half
-      value = initialL[index] 
       
+      #We pick a random position on this range  
+                       
+      random = int(randrange(lenL-1))
+      
+      index = indexSlicers[0] + random
+      value = initialL[index] 
+      conditions=[]
        
-      IfBigger = (n > value)
-      IfEqual = (n == value)
-      IfLess = (n < value)
+      IfBigger = ( value < n)
+      IfEqual = (value == n)
+      IfLess = ( value > n)
       
       if debug:
-       conditions=[]
+        
        conditions.append(IfBigger)
        conditions.append(IfEqual)
        conditions.append(IfLess)
-       print( "lenL : {}, n: {} \n Conditions: {} \n Value :  {}\n Index :  {}  \n Slicing index : {} \n".format(lenL,n,conditions,value,index,indexSlicers))
-      
-      if IfEqual:    
-         return  index   
+       print( "lenL : {}, n: {} \n Conditions: {} \n Value :  {}\n Index :  {}  \n Slicing index : {} \n".format(lenL,n,conditions,value,index,indexSlicers) )
+  
+      if IfEqual:
+    
+         return  index
+        
       if IfBigger:
+        
          indexSlicers[0] = index          
          l = initialL[indexSlicers[0]:indexSlicers[1]]
          lenL = len(l)
          
-         return findNearNumberbyHalfingSignComparison(d,n, lenL,indexSlicers, initialL, debug )  
+         return FindNearNumberbyHalfingSignComparison(d,n, lenL,indexSlicers, initialL, debug )
+      
+      #and slice the parts from the array that are   
 
       if IfLess:
          indexSlicers[1] = index                   
          l = initialL[indexSlicers[0]:indexSlicers[1]]
          lenL = len(l)
-         return findNearNumberbyHalfingSignComparison(d,n , lenL ,indexSlicers, initialL, debug)
-
-    return findNearNumberbyHalfingSignComparison(d, n, lenL, indexSlicers, initialL, debug )
-    
+         return FindNearNumberbyHalfingSignComparison(d,n , lenL ,indexSlicers, initialL, debug)
+      
+      
+    return FindNearNumberbyHalfingSignComparison(d, n, lenL, indexSlicers, initialL, debug )
+   
 def primes(n):
    # https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
        
@@ -117,21 +133,23 @@ def goldbachConjecture(n, p, debug=False):
   removeMax = False   
   maxP=[]  
   nStart = n  
-  def golbachConjecturePairs( p, loopIter = 0, n = 0 , maxP=[] , pSum=[], removeMax = False , nStart = 0 , debug=False ):     
-     sliceN = equalorSmallerIndexOnListToN(n,p,debug)
-     sliceN2 = sliceN+1
-     pList = p[:sliceN2]     
+  
+  def goldbachConjecturePairs( p, loopIter = 0, n = 0 , maxP=[] , pSum=[], removeMax = False , nStart = 0 , debug=False ):     
+     sliceN = equalOrSmallerIndexOnListToN(n,p,debug)
+     
+     pList = p[:sliceN+1]     
      if debug :       
        print("n {}\n loopiter: {}\n removeMax: {} \n".format(n,loopIter,removeMax))          
      if debug :       
-       print("slice index: {}\n prime List : {}\n sliced Prime List: {} \n".format(sliceN,p, pList))  
+       print("slice index: {}\n slice index2: {}\n prime List : {}\n sliced Prime List: {} \n".format(sliceN ,p, pList))  
      #we get ride of any MaxPrime who end up with a 1 at the end of the sum  
      if removeMax == True:
        #only if Previous Solution didn't work we remove previous MAX     
        pList = [i for i in pList if i not in maxP]     
        if debug :           
         print("prime List with Max removed: {} \n, maxP: {} \n: ".format(pList, maxP))      
-     pListSort = sorted(pList,reverse=True)     
+     pListSort = sorted(pList,reverse=True)    
+      
      if debug :     
       print("Sorted Prime List: {} \n".format(pListSort))    
      p1 = int(pListSort[0]) 
@@ -168,11 +186,15 @@ def goldbachConjecture(n, p, debug=False):
            loopIter = 0
            pSum = [] #reset the sum 
            removeMax = True #but remove the latest MaxP we added
-           return golbachConjecturePairs(p, loopIter, nStart , maxP , pSum, removeMax,nStart,debug)      
+           return goldbachConjecturePairs(p, loopIter, nStart , maxP , pSum, removeMax,nStart,debug)      
          if len(pSum) == 2:      
            # This is our Valid Pair, got it!.
-           return pSum   
-  return  golbachConjecturePairs(p, loopIter,n,maxP,pSum,removeMax ,nStart,debug)
+           MaxPLen= int(len(maxP))
+           print("n: {}\nMaxP : {} \nPsum: {}\n".format(nStart,MaxPLen,pSum))
+           
+           return pSum
+         
+  return  goldbachConjecturePairs(p, loopIter,n,maxP,pSum,removeMax ,nStart,debug)
 
 def goldbachConjecturePair(n:int,debug=False):
   
@@ -181,24 +203,27 @@ def goldbachConjecturePair(n:int,debug=False):
    p = primes(n)
    prime_toc = time.perf_counter()
    primeTime = prime_toc - prime_tic
+   print("prime list runTime: {} \n".format(primeTime))
    n = list(range(2, n+1,2))
    nAndPairs=[]
    runTime=[]
+   
    for e,x in enumerate(n):
      gold_tic = time.perf_counter()   
-     x1 = goldbachConjecture(x ,p, debug)
+     x1 = goldbachConjecture(x,p,debug)
      gold_toc = time.perf_counter()
      itemTime = gold_toc - gold_tic
      runTime.append(itemTime)
      nAndPairs.append(x)
      nAndPairs.append(x1)
+     
      if debug:
-      print("\n Number : {} \n Pairs = {}\n RunTime = {} Secs \n ".format(x,x1,itemTime)) 
-   
-   total = sum(runTime)                  
-   print("total time of GoldbachF : {} \n Prime F time: {}".format(total,primeTime))              
+      print("\n Number : {} \n Pairs = {}\n RunTime = {} Secs \n MaxP {} \n".format(x,x1,itemTime))                     
+              
    return runTime,nAndPairs      
              
-n = 100000
+n = 10000
 
-print(goldbachConjecturePair(n , debug=False))
+t,p = goldbachConjecturePair(n , debug=False)
+
+print(" \n total Run Time: {} seconds \n".format(sum(t)))
